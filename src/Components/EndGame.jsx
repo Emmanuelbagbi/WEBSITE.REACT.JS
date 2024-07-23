26357dc4ad247f9920d2618a1df36011660a6064
Emmanuelbagbi <emmanuelbagbi4@gmail.com>
2024-07-23 21:31:29 +0100

    initial commit

--- src/Components/EndGame.jsx
+++ src/Components/EndGame.jsx
@@ -1,28 +1,29 @@
 
 
-function EndGame(params) {
+// eslint-disable-next-line no-unused-vars
+function EndGame(_params) {
     return(
         <>
             <section className="copyright container-fluid d-flex1 align-items-center	justify-content-between tx-14">
-            <div class="copyright-text">
+            <div className="copyright-text">
                  <p>
-            © <span class="date">2024</span> Access Bank PLC. <span>(Licensed by the Central Bank of Nigeria)</span>
+            © <span className="date">2024</span> Access Bank PLC. <span>(Licensed by the Central Bank of Nigeria)</span>
             </p>
             </div>
 
-            <div class="copyright-links">
-            <ul class="d-flex flex-wrap">
+            <div className="copyright-links">
+            <ul className="d-flex flex-wrap">
                     <li>
-                        <a href="/accessibility" class="mx-3 link">Accessibility</a>
+                        <a href="/accessibility" className="mx-3 link">Accessibility</a>
                     </li>
                     <li>
-                        <a href="/privacy-policy" class="mx-3 link">Privacy Policy</a>
+                        <a href="/privacy-policy" className="mx-3 link">Privacy Policy</a>
                     </li>
                     <li>
-                        <a href="/terms-of-use" class="mx-3 link">Terms of Use</a>
+                        <a href="/terms-of-use" className="mx-3 link">Terms of Use</a>
                     </li>
                     <li>
-                        <a href="/customer-support/whistle-blower" class="mx-3 link">Whistle Blower</a>
+                        <a href="/customer-support/whistle-blower" className="mx-3 link">Whistle Blower</a>
                     </li>
             </ul>
             </div>
@@ -30,12 +31,12 @@ function EndGame(params) {
 
 
 
-            <div class="logo-and-text align-items-center d-flex1">
-            <img src="src/assets/Image/access-logo.png" class="logo1" alt=""/>
+            <div className="logo-and-text align-items-center d-flex1">
+            <img src="src/assets/Image/access-logo.png" className="logo1" alt=""/>
             <svg width="19" height="50" viewBox="0 0 19 52" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.4054 0H19L3.59459 52H0L15.4054 0Z" fill="#F98537"></path>
             </svg>
-            <p class="text mb-0 ml-1 font-weight-bold text-orange">
+            <p className="text mb-0 ml-1 font-weight-bold text-orange">
             more than banking
             </p>
             </div>
